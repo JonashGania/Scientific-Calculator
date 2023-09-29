d38fdc05405e5a5d339f130466f265cff0e3c6d4
Jonash Gania <jonashtiglao@gmail.com>
2023-09-29 23:23:59 +0800

    add button objects and click event

---
+++ src/index.js
@@ -0,0 +1,217 @@
+const Operators = ['+', '-', '*', '/'];
+const POWER = "POWER(", FACTORIAL = "FACTORIAL";
+
+let calcu_buttons = [
+    {
+        name: 'sin',
+        symbol: 'sin',
+        formula: 'trigo(Math.sin,',
+        type: 'trigonometry'
+    },
+    {
+        name: 'cos',
+        symbol: 'cos',
+        formula: 'trigo(Math.cos,',
+        type: 'trigonometry'
+    },
+    {
+        name: 'tan',
+        symbol: 'tan',
+        formula: 'trigo(Math.tan,',
+        type: 'trigonometry'
+    },
+    {
+        name: 'open-parenthesis',
+        symbol: '(',
+        formula: '(',
+        type: 'number'
+    },
+    {
+        name: 'close-parenthesis',
+        symbol: ')',
+        formula: ')',
+        type: 'number'
+    },
+    {
+        name: 'log',
+        symbol: 'log',
+        formula: 'Math.log10',
+        type: 'math_function'
+    },
+    {
+        name: 'factorial',
+        symbol: '!',
+        formula: FACTORIAL,
+        type: 'math_function'
+    },
+    {
+        name: 'square-root',
+        symbol: '√',
+        formula: 'Math.sqrt',
+        type: 'math_function'
+    },
+    {
+        name: 'exponentiation',
+        symbol: 'e',
+        formula: 'Math.E',
+        type: 'number'
+    },
+    {
+        name: 'power',
+        symbol: 'x<sup>y</sup>',
+        formula: POWER,
+        type: 'math_function'
+    },
+    {
+        name: 'seven',
+        symbol: 7,
+        formula: 7,
+        type: 'number'
+    },
+    {
+        name: 'eight',
+        symbol: 8,
+        formula: 8,
+        type: 'number'
+    },
+    {
+        name: 'nine',
+        symbol: 9,
+        formula: 9,
+        type: 'number'
+    },
+    {
+        name: 'delete',
+        symbol: 'DEL',
+        formula: false,
+        type: 'key'
+    },
+    {
+        name: 'all-clear',
+        symbol: 'AC',
+        formula: false,
+        type: 'key'
+    },
+    {
+        name: 'four',
+        symbol: 4,
+        formula: 4,
+        type: 'number'
+    },
+    {
+        name: 'five',
+        symbol: 5,
+        formula: 5,
+        type: 'number'
+    },
+    {
+        name: 'six',
+        symbol: 6,
+        formula: 6,
+        type: 'number'
+    },
+    {
+        name: 'multiplication',
+        symbol: '×',
+        formula: '*',
+        type: 'operator'
+    },
+    {
+        name: 'division',
+        symbol: '÷',
+        formula: '/',
+        type: 'operator'
+    },
+    {
+        name: 'one',
+        symbol: 1,
+        formula: 1,
+        type: 'number'
+    },
+    {
+        name: 'two',
+        symbol: 2,
+        formula: 2,
+        type: 'number'
+    },
+    {
+        name: 'three',
+        symbol: 3,
+        formula: 3,
+        type: 'number'
+    },
+    {
+        name: 'addition',
+        symbol: '+',
+        formula: '+',
+        type: 'operator'
+    },
+    {
+        name: 'subtraction',
+        symbol: '-',
+        formula: '-',
+        type: 'operator'
+    },
+    {
+        name: 'zero',
+        symbol: 0,
+        formula: 0,
+        type: 'number'
+    },
+    {
+        name: 'pi',
+        symbol: 'π',
+        formula: 'Math.PI',
+        type: 'number'
+    },
+    {
+        name: 'percent',
+        symbol: '&',
+        formula: '/100',
+        type: 'number'
+    },
+    {
+        name: 'dot',
+        symbol: '.',
+        formula: '.',
+        type: 'number'
+    },
+    {
+        name: 'equal',
+        symbol: '=',
+        formula: '=',
+        type: 'calculate'
+    }
+]
+
+
+
+const inputElement = document.querySelectorAll('.input');
+
+inputElement.forEach(input => {
+    input.addEventListener('click', (event) => {
+        const targetBtn = event.target;
+
+        calcu_buttons.forEach(button => {
+            if(button.name == targetBtn.id) calculator(button);
+        })
+    })
+})
+
+
+function calculator(button){
+    if(button.type === 'operator'){
+
+    } else if (button.type === 'trigonometry'){
+
+    } else if (button.type === 'number'){
+        
+    } else if (button.type === 'math_function'){
+        
+    } else if (button.type === 'key'){
+        
+    } else if (button.type === 'calculate'){
+        
+    }
+}
+
