061240102f747f61ae081f68d53502b05b712cd8
Jonash Gania <jonashtiglao@gmail.com>
2023-10-05 22:11:06 +0800

    import module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import calculator from "./calculatorLogic";
 import { calcuButtons } from "./calculatorLogic";
+import calculate from "./calculatorCli";
 
 // Buttons click event
 function onButtonClick(){
@@ -59,3 +60,4 @@ function handleKeyboardInput(event) {
 
 document.addEventListener('keydown', handleKeyboardInput);
 onButtonClick();
+calculate();
