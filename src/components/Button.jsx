eb970f830b2b98a6725b7e54e4863b633d848a28
Jonash Gania <jonashtiglao@gmail.com>
2023-11-10 22:31:01 +0800

    added button scale

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,7 @@ export default function Button({button, calculator}) {
     return (
         <button 
             onClick={onButtonClick}
-            className='text-lg w-10 h-8 mobile:w-16 mobile:h-12 bg-slate-300 rounded-md font-semibold shadow-button'
+            className='text-lg w-10 h-8 mobile:w-16 mobile:h-12 bg-slate-300 rounded-md font-semibold shadow-button transition-all duration-200 ease-in active:scale-[0.7]'
         >
             {button.symbol}
         </button>
